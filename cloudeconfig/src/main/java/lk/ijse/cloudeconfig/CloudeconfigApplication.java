ce593d661ab35f01356c0825ccd9bca16a7e141d
Dilsha Pawani <dilshaperera1118@gmail.com>
2025-06-25 20:06:28 +0530

    cloude config updated

---
+++ cloudeconfig/src/main/java/lk/ijse/cloudeconfig/CloudeconfigApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cloudeconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudeconfigApplication.class, args);
+    }
+
+}
---
+++ cloudeconfig/src/test/java/lk/ijse/cloudeconfig/CloudeconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cloudeconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
